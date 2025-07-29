80c7e9f324a7ff3b43c8cc0ed109e7b55e63ce66
rockey0760995443 <161614732+rockey0760995443@users.noreply.github.com>
2025-07-29 14:59:13 +0200

    Update myList.py

--- myList.py
+++ myList.py
@@ -25,6 +25,4 @@ print("Index of value 30:", index_30)
 
 print("Final list:", my_list)
 
-=============== RESTART: C:/Users/98071456/Desktop/plp/myList.py ===============
-Index of value 30: 3
-Final list: [10, 15, 20, 30, 40, 50, 60]
+
